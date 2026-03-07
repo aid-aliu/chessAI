439b42448b174822b506e717c36fb62703fc7869
Aid Aliu <aid.aliu@genpact.com>
2026-03-07 03:55:34 +0100

    very basic piece movement

--- chess/board.py
+++ chess/board.py
@@ -1,6 +1,6 @@
 class Board:
     def __init__(self):
-        self.chess_board = [[0 for i in range(8)] for j in range(8)]
+        self.board = [[0 for i in range(8)] for j in range(8)]
 
         self.pawn = 1
         self.rook = 2
@@ -9,34 +9,65 @@ class Board:
         self.king = 5
         self.queen = 6
 
+        #black turn is when white_turn is false
+        self.white_turn = True
+
         # pawns
         for i in range(8):
-            self.chess_board[1][i] = -self.pawn
-            self.chess_board[6][i] = self.pawn
+            self.board[1][i] = -self.pawn
+            self.board[6][i] = self.pawn
 
         # rooks
-        self.chess_board[0][0], self.chess_board[0][7] = -self.rook, -self.rook
-        self.chess_board[7][0], self.chess_board[7][7] = self.rook, self.rook
+        self.board[0][0], self.board[0][7] = -self.rook, -self.rook
+        self.board[7][0], self.board[7][7] = self.rook, self.rook
 
         # knights
-        self.chess_board[0][1], self.chess_board[0][6] = -self.knight, -self.knight
-        self.chess_board[7][1], self.chess_board[7][6] = self.knight, self.knight
+        self.board[0][1], self.board[0][6] = -self.knight, -self.knight
+        self.board[7][1], self.board[7][6] = self.knight, self.knight
 
         # bishops
-        self.chess_board[0][2], self.chess_board[0][5] = -self.bishop, -self.bishop
-        self.chess_board[7][2], self.chess_board[7][5] = self.bishop, self.bishop
+        self.board[0][2], self.board[0][5] = -self.bishop, -self.bishop
+        self.board[7][2], self.board[7][5] = self.bishop, self.bishop
 
         # queens
-        self.chess_board[0][3] = -self.queen
-        self.chess_board[7][3] = self.queen
+        self.board[0][3] = -self.queen
+        self.board[7][3] = self.queen
 
         # kings
-        self.chess_board[0][4] = -self.king
-        self.chess_board[7][4] = self.king
+        self.board[0][4] = -self.king
+        self.board[7][4] = self.king
+
 
     def print_board(self):
         for i in range(8):
-            print(self.chess_board[i])
+            print(self.board[i])
 
 
+    def move_piece(self, row, col, row_move, col_move):
+
+        if row < 0 or row >= 8 or col < 0 or col >= 8:
+            print("Position out of bounds")
+            return False
+        if row_move < 0 or row_move >= 8 or col_move < 0 or col_move >= 8:
+            print("Position out of bounds")
+            return False
+
+
+        if (self.board[row][col] > 0 >= self.board[row_move][col_move]
+                and self.white_turn):
+            temp = self.board[row][col]
+            self.board[row_move][col_move] = temp
+            self.board[row][col] = 0
+            self.white_turn = False
+            return True
+
+        elif (self.board[row][col] < 0 <= self.board[row_move][col_move]
+              and not self.white_turn):
+            temp = self.board[row][col]
+            self.board[row_move][col_move] = temp
+            self.board[row][col] = 0
+            self.white_turn = True
+            return True
+
+        return False
 
--- chess/chess.py
+++ chess/chess.py
@@ -2,3 +2,15 @@ from board import Board
 
 board = Board()
 board.print_board()
+
+# 0 empty, 1 pawn, 2 rook, 3 knight, 4 bishop, 5 king, 6 queen
+
+
+print("=============================================")
+board.move_piece(6, 0, 5, 0)
+board.print_board()
+
+print("=============================================")
+board.move_piece(1, 0, 2, 0)
+board.print_board()
+
